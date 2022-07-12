2c0af4a74baac267e91affe88af494fccdb3a4f5
Wei Zhu <yesmeck@gmail.com>
2022-07-12 20:48:07 +0930

    feat: support new remix version

--- src/cli.ts
+++ src/cli.ts
@@ -3,8 +3,15 @@ import meow from 'meow';
 import * as fs from 'fs';
 import * as path from 'path';
 import chokidar from 'chokidar';
-import type { ConfigRoute } from '@remix-run/dev/config/routes';
-import { readConfig } from '@remix-run/dev/config';
+import type { ConfigRoute } from '@remix-run/dev/dist/config/routes';
+
+let readConfig: typeof import('@remix-run/dev/dist/config').readConfig;
+
+try {
+  readConfig = require('@remix-run/dev/config').readConfig;
+} catch (e) {
+  readConfig = require('@remix-run/dev/dist/config').readConfig;
+}
 
 const helpText = `
 Usage
@@ -17,10 +24,10 @@ Options
 const cli = meow(helpText, {
   flags: {
     watch: {
-      type: "boolean",
-      alias: "w"
-    }
-  }
+      type: 'boolean',
+      alias: 'w',
+    },
+  },
 });
 
 type RoutesInfo = Record<string, string[]>;
@@ -28,21 +35,27 @@ type RoutesInfo = Record<string, string[]>;
 async function buildHelpers(remixRoot: string): Promise<RoutesInfo> {
   const config = await readConfig(remixRoot);
   const routesInfo: RoutesInfo = {};
-  const handleRoutesRecursive = (parentId?: string, parentPath: ConfigRoute[] = []) => {
+  const handleRoutesRecursive = (
+    parentId?: string,
+    parentPath: ConfigRoute[] = [],
+  ) => {
     let routes = Object.values(config.routes).filter(
-      route => route.parentId === parentId
+      (route) => route.parentId === parentId,
     );
-    routes.forEach(route => {
-      let currentPath = parentPath
+    routes.forEach((route) => {
+      let currentPath = parentPath;
       if (route.path) {
-        currentPath = [...currentPath, route]
-        const fullPath = currentPath.reduce((acc, curr) => [acc, curr.path].join('/'), '');
+        currentPath = [...currentPath, route];
+        const fullPath = currentPath.reduce(
+          (acc, curr) => [acc, curr.path].join('/'),
+          '',
+        );
         const paramsNames = parse(currentPath);
         routesInfo[fullPath] = paramsNames;
       }
       handleRoutesRecursive(route.id, currentPath);
     });
-  }
+  };
   handleRoutesRecursive();
   return routesInfo;
 }
@@ -54,37 +67,49 @@ export async function build(remixRoot: string) {
 
 function watch(remixRoot: string) {
   build(remixRoot);
-  chokidar.watch([path.join(remixRoot, 'app/routes/**/*.{ts,tsx}'), path.join(remixRoot, 'remix.config.js')]).on('change', () => {
-    build(remixRoot);
-  });
+  chokidar
+    .watch([
+      path.join(remixRoot, 'app/routes/**/*.{ts,tsx}'),
+      path.join(remixRoot, 'remix.config.js'),
+    ])
+    .on('change', () => {
+      build(remixRoot);
+    });
   console.log('Watching for routes changes...');
 }
 
 function generate(routesInfo: RoutesInfo) {
   const jsCode = generateHelpers(routesInfo);
-  const tsCode = [
-    generatePathDefinition(routesInfo),
-    generateParamsDefinition(routesInfo)
-  ].join('\n\n') + '\n\n';
+  const tsCode =
+    [
+      generatePathDefinition(routesInfo),
+      generateParamsDefinition(routesInfo),
+    ].join('\n\n') + '\n\n';
   const outputPath = path.join(process.cwd(), 'node_modules', '.remix-routes');
   if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath);
   }
   fs.writeFileSync(path.join(outputPath, 'index.js'), jsCode);
   fs.writeFileSync(path.join(outputPath, 'types.d.ts'), tsCode);
-  fs.writeFileSync(path.join(outputPath, 'package.json'), JSON.stringify({
-    "name": ".remix-routes",
-    "main": "index.js",
-  }));
+  fs.writeFileSync(
+    path.join(outputPath, 'package.json'),
+    JSON.stringify({
+      name: '.remix-routes',
+      main: 'index.js',
+    }),
+  );
 }
 
 function generateHelpers(routesInfo: RoutesInfo) {
-  const routes = Object.entries(routesInfo).reduce((acc: Record<string, string[]>, [route, params]) => {
-    if (params.length > 0) {
-      acc[route] = params;
-    }
-    return acc;
-  }, {})
+  const routes = Object.entries(routesInfo).reduce(
+    (acc: Record<string, string[]>, [route, params]) => {
+      if (params.length > 0) {
+        acc[route] = params;
+      }
+      return acc;
+    },
+    {},
+  );
   return `
 const routes = ${JSON.stringify(routes, null, 2)};
 
@@ -98,13 +123,15 @@ function generatePathDefinition(routesInfo: RoutesInfo) {
     '/': [],
     ...routesInfo,
   }).forEach(([route, paramsNames]) => {
-    const lines = ['export declare function $path(']
-    lines.push(`  route: ${JSON.stringify(route)},`)
+    const lines = ['export declare function $path('];
+    lines.push(`  route: ${JSON.stringify(route)},`);
     if (paramsNames.length > 0) {
-      const paramsType = paramsNames.map(paramName => `${paramName}: string | number`);
-      lines.push(`  params: { ${paramsType.join('; ')} },`)
+      const paramsType = paramsNames.map(
+        (paramName) => `${paramName}: string | number`,
+      );
+      lines.push(`  params: { ${paramsType.join('; ')} },`);
     }
-    lines.push(`  query?: Record<string, string | number>`)
+    lines.push(`  query?: Record<string, string | number>`);
     lines.push(`): string;`);
     code.push(lines.join('\n'));
   });
@@ -112,25 +139,29 @@ function generatePathDefinition(routesInfo: RoutesInfo) {
 }
 
 function generateParamsDefinition(routesInfo: RoutesInfo) {
-  const routes = Object.entries(routesInfo)
+  const routes = Object.entries(routesInfo);
 
   // $params helper makes sense only for routes with params.
-  const routesWithParams = routes.filter(([_, paramsNames]) => paramsNames.length > 0)
+  const routesWithParams = routes.filter(
+    ([_, paramsNames]) => paramsNames.length > 0,
+  );
 
   const code = routesWithParams.map(([route, paramsNames]) => {
-    const lines: string[] = []
-    
-    lines.push(`export declare function $params(`)
-    lines.push(`  route: ${JSON.stringify(route)},`)
-    lines.push(`  params: { readonly [key: string]: string | undefined }`)
-    lines.push(`): {`)
-    lines.push(paramsNames.map(paramName => `  ${paramName}: string`).join(',\n'))
-    lines.push(`};`)
+    const lines: string[] = [];
 
-    return lines.join('\n')
-  })
+    lines.push(`export declare function $params(`);
+    lines.push(`  route: ${JSON.stringify(route)},`);
+    lines.push(`  params: { readonly [key: string]: string | undefined }`);
+    lines.push(`): {`);
+    lines.push(
+      paramsNames.map((paramName) => `  ${paramName}: string`).join(',\n'),
+    );
+    lines.push(`};`);
 
-  return code.join('\n')
+    return lines.join('\n');
+  });
+
+  return code.join('\n');
 }
 
 function parse(routes: ConfigRoute[]) {
