43427b6863a9b303943a11bf714d822218a0034e
Wei Zhu <yesmeck@gmail.com>
2023-06-02 17:17:41 +0930

    test: add test case for v2 routes (#52)

--- packages/remix-routes/src/__tests__/cli.test.ts
+++ packages/remix-routes/src/__tests__/cli.test.ts
@@ -3,9 +3,16 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { build } from '../cli';
 
-test('build', async () => {
-  await build(path.resolve(__dirname, '../../fixture'), './node_modules');
+test('build v1 routes', async () => {
+  await build(path.resolve(__dirname, '../../fixture/v1'), './node_modules');
   expect(
-    fs.readFileSync(path.resolve(__dirname, '../../fixture/node_modules/remix-routes.d.ts'), 'utf8'),
+    fs.readFileSync(path.resolve(__dirname, '../../fixture/v1/node_modules/remix-routes.d.ts'), 'utf8'),
+  ).toMatchSnapshot();
+});
+
+test('build v2 routes', async () => {
+  await build(path.resolve(__dirname, '../../fixture/v2'), './node_modules');
+  expect(
+    fs.readFileSync(path.resolve(__dirname, '../../fixture/v2/node_modules/remix-routes.d.ts'), 'utf8'),
   ).toMatchSnapshot();
 });
