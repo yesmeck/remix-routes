a18ad4210ac4c83a8698a3af85896666507e4dcb
Wei Zhu <yesmeck@gmail.com>
2024-02-13 10:22:43 +1030

    fix: vite plugin type

--- packages/remix-routes/vite.d.ts
+++ packages/remix-routes/vite.d.ts
@@ -1,1 +1,7 @@
-export declare function remixRoutes(): Vite.Plugin[];
+import type * as Vite from 'vite';
+interface PluginConfig {
+  strict?: boolean;
+  outDir?: string;
+}
+
+export declare function remixRoutes(pluginConfig: PluginConfig = {}): Vite.Plugin;
