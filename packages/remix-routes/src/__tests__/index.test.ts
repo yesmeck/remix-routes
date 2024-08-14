7331dc4a1c8c58f1aed8327e37fdea53c2fd63f8
Max Schmidt <me@mooxl.de>
2024-08-14 12:00:57 +0200

    add tests
    Signed-off-by: Max Schmidt <me@mooxl.de>

--- packages/remix-routes/src/__tests__/index.test.ts
+++ packages/remix-routes/src/__tests__/index.test.ts
@@ -1,53 +1,66 @@
-import { test, expect } from 'vitest';
-import { $path, $params } from '..';
+import { expect, test } from "vitest";
+import { $params, $path } from "..";
 
-test('$path', () => {
-  expect($path('/posts')).toBe('/posts');
+test("$path", () => {
+	expect($path("/posts")).toBe("/posts");
 });
 
-test('$path + params', () => {
-  expect($path('/posts/:id', { id: 1 })).toBe('/posts/1');
+test("$path + params", () => {
+	expect($path("/posts/:id", { id: 1 })).toBe("/posts/1");
 });
 
-test('$path + params + escape periods', () => {
-  expect($path('/posts/:id.json', { id: 1 })).toBe('/posts/1.json');
+test("$path + params + escape periods", () => {
+	expect($path("/posts/:id.json", { id: 1 })).toBe("/posts/1.json");
 });
 
-test('$path + query', () => {
-  expect($path('/posts', { order: 'desc' })).toBe('/posts?order=desc');
+test("$path + query", () => {
+	expect($path("/posts", { order: "desc" })).toBe("/posts?order=desc");
 });
 
-test('$path + array query', () => {
-  expect(
-    $path('/posts/delete', [
-      ['id', '1'],
-      ['id', '2'],
-    ]),
-  ).toBe('/posts/delete?id=1&id=2');
+test("$path + array query", () => {
+	expect(
+		$path("/posts/delete", [
+			["id", "1"],
+			["id", "2"],
+		]),
+	).toBe("/posts/delete?id=1&id=2");
+	expect($path("/posts/delete", { id: ["1", "2"] })).toBe(
+		"/posts/delete?id=1&id=2",
+	);
 });
 
-test('$path + params + query', () => {
-  expect($path('/posts/:id', { id: 1 }, { raw: 'true' })).toBe(
-    '/posts/1?raw=true',
+test('$path + undefined or null queries', () => {
+  expect($path('/posts', { order: undefined })).toBe('/posts');
+  expect($path('/posts', { order: undefined, filter: 'draft' })).toBe(
+    '/posts?filter=draft'
+  );
+  expect($path('/posts', { order: undefined, filter: null, isDraft: false })).toBe(
+    '/posts?isDraft=false'
   );
 });
 
-test('$path + optional route fragment', () => {
-  expect($path('/:lang?/about', {})).toBe('/about');
-  expect($path('/:lang?/about', { lang: undefined })).toBe('/about');
-  expect($path('/:lang?/about', { lang: 'en' })).toBe('/en/about');
+test("$path + params + query", () => {
+	expect($path("/posts/:id", { id: 1 }, { raw: "true" })).toBe(
+		"/posts/1?raw=true",
+	);
 });
 
-test('$path + catch all route', () => {
-  expect($path("/sign-in/*", { "*": "admin" })).toBe("/sign-in/admin");
-  expect($path("/sign-in/*")).toBe("/sign-in");
-  expect($path("/sign-in/*", { "*": "" })).toBe("/sign-in/");
+test("$path + optional route fragment", () => {
+	expect($path("/:lang?/about", {})).toBe("/about");
+	expect($path("/:lang?/about", { lang: undefined })).toBe("/about");
+	expect($path("/:lang?/about", { lang: "en" })).toBe("/en/about");
 });
 
-test('optional segment', () => {
-  expect($path('/tree?/:tree?', { tree: 'main' })).toBe('/tree/main');
+test("$path + catch all route", () => {
+	expect($path("/sign-in/*", { "*": "admin" })).toBe("/sign-in/admin");
+	expect($path("/sign-in/*")).toBe("/sign-in");
+	expect($path("/sign-in/*", { "*": "" })).toBe("/sign-in/");
 });
 
-test('$params', () => {
-  expect($params('/posts/:id', { id: '1' })).toStrictEqual({ id: '1' });
+test("optional segment", () => {
+	expect($path("/tree?/:tree?", { tree: "main" })).toBe("/tree/main");
+});
+
+test("$params", () => {
+	expect($params("/posts/:id", { id: "1" })).toStrictEqual({ id: "1" });
 });
